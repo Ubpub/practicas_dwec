007b9864144e83d67546c5ad5ed0f1ef6665a770
Cecilia Llamazares Lopez <ceciliallamazares99@gmail.com>
2022-09-28 13:26:40 +0200

    Práctica 8

--- js/scriptpr0207.js
+++ js/scriptpr0207.js
@@ -1,5 +1,8 @@
-let numero = prompt("Introduce un número");
 let asterisco = "*"
+let numero = prompt("Introduce un número");
+while(isNaN(numero) || numero == "" || numero == null || numero < 0){
+    numero = prompt("Introduce un número");
+}
 for(let i = 0; i < numero; i++) {
     console.log(asterisco);
     asterisco = asterisco + "*";
---
+++ js/scriptpr0208.js
@@ -0,0 +1,18 @@
+let ano = prompt("Introduce tu año de nacimiento: ");
+
+while(isNaN(ano) || ano == "" || ano == null || ano < 0) {
+    ano = prompt("Introduce tu año de nacimiento: ");
+}
+
+if(ano < 1949)
+    alert("Tu generación es anterior a la de Baby Boomer");
+else if (ano <= 1968)
+    alert("Tu generación es Baby Boomer");
+else if (ano <= 1980)
+    alert("Tu generación es Generación X");
+else if (ano <= 1993)
+    alert("Tu generación es Generación Millenial");
+else if (ano <= 2010)
+    alert("Tu generación es Generación Z");
+else
+    alert("Tu generación es posterior a Generación Z");
