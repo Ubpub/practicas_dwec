8501b0ce10795f5e84ccbe7da7cd2be8fd1c6c8e
Cecilia Llamazares Lopez <ceciliallamazares99@gmail.com>
2022-09-30 11:00:50 +0200

    Cambio en la práctica 11

--- js/scriptpr0211.js
+++ js/scriptpr0211.js
@@ -2,8 +2,8 @@ let numero = prompt("Introduce un núemro");
 while(isNaN(numero) || numero == "" || numero == null || numero <= 0) {
     numero = prompt("Introduce un número");
 }
-let resultado = "El número es impar";
+let resultado = "impar";
 if((numero % 2) == 0) {
-    resultado = "El número es par";
+    resultado = "par";
 }
-alert(resultado);
+alert(`El número es ${ resultado }`);
