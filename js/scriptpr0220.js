21c1c40913a9ba64539239c622b65a86cdbd8327
Cecilia Llamazares Lopez <ceciliallamazares99@gmail.com>
2022-10-07 10:48:13 +0200

    Práctica 20

--- js/scriptpr0220.js
+++ js/scriptpr0220.js
@@ -2,6 +2,18 @@ let numero = prompt("Introduce un número:");
 console.log(getFibonacciAt(numero));
 
 function getFibonacciAt(n) {
-    if (n <= 1) return n;
-    return (n - 1) + (n - 2);
+    if (n < 0) return "Es imposible";
+    else if (n <= 1) return n;
+    else {
+        let num1 = 0;
+        let num2 = 1;
+        let suma = 0;
+        for (let i = 0; i < n; i++) {
+            suma = num1 + num2;
+            num1 = num2;
+            num2 = suma;
+        }
+        return num2;
+    }
+
 }
