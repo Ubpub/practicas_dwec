4cec88475a66324e3ab5bfc6c300bdabe828758d
Cecilia Llamazares Lopez <ceciliallamazares99@gmail.com>
2022-10-07 10:58:36 +0200

    Práctica 21 y cambios en la lista

--- js/scriptpr0221.js
+++ js/scriptpr0221.js
@@ -1,5 +1,8 @@
 let cadena = prompt("Introduce una cadena para comprobar si es un número:");
+console.log(isNumber(cadena));
 
 function isNumber(str) {
-    // if (Number())
+    // if (!isNaN(str)) return true;
+    // else return false;
+    return (!isNaN(str));
 }
