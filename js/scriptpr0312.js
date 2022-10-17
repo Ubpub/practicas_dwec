6da5a3ac7bc537e797f3c7e981601071a0d15241
Cecilia Llamazares Lopez <ceciliallamazares99@gmail.com>
2022-10-17 09:23:06 +0200

    Unidad 3 práctica 12

--- js/scriptpr0312.js
+++ js/scriptpr0312.js
@@ -1,1 +1,21 @@
-// Se puede utilizar un objeto.
+console.log(isHeterograma("Villabalter"));
+
+function isHeterograma(str) {
+    let letras = countLetters(str);
+    for (key in letras) {
+        if (letras[key] > 1) return false;
+    }
+    return true;
+}
+
+function countLetters(str) {
+    str = str.toLowerCase();
+    let letras = {};
+    for (let char of str) { // letras.char = 0;
+        if (char != "," && char != " ") {
+            if(letras[char] >= 1) letras[char] += 1;
+            else letras[char] = 1;
+        }
+    }
+    return letras;
+}
