97c5c301491f0e420605a3f1c2d0269618ebf03d
Cecilia Llamazares Lopez <ceciliallamazares99@gmail.com>
2022-10-17 08:58:18 +0200

    Unidad 3 práctica 6

--- js/scriptpr0306.js
+++ js/scriptpr0306.js
@@ -1,9 +1,19 @@
 let str = '100101';
 let baseFrom = 2;
 let baseTo = 16;
-console.log(str, baseFrom, baseTo)
+console.log(convertBase(str, baseFrom, baseTo))
 
 function convertBase(str, baseFrom, baseTo) {
-    let num = parseInt(str, baseFrom);
-    return parseInt(num, baseTo);
+    // let num = parseInt(str, baseFrom);
+    if (baseFrom == 2) {
+        return parseInt(str, baseTo);
+    } else if (baseFrom == 10) {
+        if (baseTo == 2) {
+            return str.toString(baseTo);
+        }
+        return parseInt(str, baseTo);
+    } else {
+        return str.tString(baseTo);
+    }
+    
 }
