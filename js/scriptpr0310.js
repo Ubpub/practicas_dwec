8aed27bf665d8b9df0e304807320012b3334bcf4
Cecilia Llamazares Lopez <ceciliallamazares99@gmail.com>
2022-10-17 10:00:55 +0200

    Cambios en la práctica 11 de la unidad 3

--- js/scriptpr0310.js
+++ js/scriptpr0310.js
@@ -1,10 +1,20 @@
-console.log(replaceWithPosition("hola amigos"));
+console.log(replaceWithPositionV2("hola amigos"));
 
 function replaceWithPosition(str) {
     str = str.toUpperCase().replace(" ", "").split("");
     let cadena = "";
     for(let char of str) {
         let pos = char.charCodeAt()-64;
+        cadena += " " + pos;
+    }
+    return cadena;
+}
+
+function replaceWithPositionV2(str) {
+    str = str.toUpperCase().replace(" ", "").split("");
+    let cadena = "";
+    for (let char of str) {
+        let pos = parseInt(char, 36) -9;
         cadena += " " + pos;
     }
     return cadena;
--- js/scriptpr0311.js
+++ js/scriptpr0311.js
@@ -3,14 +3,10 @@ console.log(ascendDescend(10, 2, 5));
 function ascendDescend(len, min, max) {
     let numero = min;
     let resultado = "";
-    let retroceso = false;
+    let retroceso = true;
     for (let i = 0; i < len; i++) {
         resultado = resultado + numero;
-        if (numero == max) {
-            retroceso = true;
-        } else if (numero == min) {
-            retroceso = false;
-        }
+        if (numero == max || numero == min) retroceso = !retroceso;
         if (retroceso) numero -= 1;
         else numero += 1;
     }
