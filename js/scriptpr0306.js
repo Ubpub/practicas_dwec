d5ea128d5f008f0aa35a37dfa054b9846f8e0bf0
Cecilia Llamazares Lopez <ceciliallamazares99@gmail.com>
2022-10-17 09:27:22 +0200

    Unidad 3 cambios en la práctica 6

--- js/scriptpr0306.js
+++ js/scriptpr0306.js
@@ -4,16 +4,6 @@ let baseTo = 16;
 console.log(convertBase(str, baseFrom, baseTo))
 
 function convertBase(str, baseFrom, baseTo) {
-    // let num = parseInt(str, baseFrom);
-    if (baseFrom == 2) {
-        return parseInt(str, baseTo);
-    } else if (baseFrom == 10) {
-        if (baseTo == 2) {
-            return str.toString(baseTo);
-        }
-        return parseInt(str, baseTo);
-    } else {
-        return str.tString(baseTo);
-    }
-    
+    let num = parseInt(str, baseFrom);
+    return num.toString(baseTo);
 }
