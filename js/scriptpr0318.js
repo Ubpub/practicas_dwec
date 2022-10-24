14b8c3822701f6e48f80297820ec214c82011ca8
Cecilia Llamazares Lopez <ceciliallamazares99@gmail.com>
2022-10-24 09:42:31 +0200

    Unidad 3 práctica 19

--- js/scriptpr0318.js
+++ js/scriptpr0318.js
@@ -7,10 +7,12 @@ let personas = [
 console.log(generateUsername(personas))
 
 function generateUsername(arr) {
-    arr.forEach( item => item["nickname"] = item.name.substring(0,1).toLowerCase() + item.surname.toLowerCase() + numeroAleatorio() + numeroAleatorio() );
+    arr.forEach( item => item["nickname"] = item.name.charAt(0).toLowerCase() + item.surname.toLowerCase() + numeroAleatorio() + numeroAleatorio() );
+    // Se puede hacer :
+    // Object.assign(item, { nickname:nickname });
     return arr;
 }
 
 function numeroAleatorio() {
-    return Math.trunc(Math.random() * 9 + 1);
+    return Math.trunc(Math.random() * 9 + 1); // Devuelve un número entre el 0 y el 10
 }
--- js/scriptpr0319.js
+++ js/scriptpr0319.js
@@ -1,3 +1,6 @@
+let numeros = [1, 6, 3, 9, 10, 7, 5];
+console.log(max(numeros));
+
 function max(arr) {
-    
+    return arr.reduce( (acum, item) => Math.max(acum, item), -Infinity );
 }
