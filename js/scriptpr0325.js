91cdc8d4b285ca058a6d9bd865293d4bbb241dec
Cecilia Llamazares Lopez <ceciliallamazares99@gmail.com>
2022-10-25 09:31:26 +0200

    Unidad 3 práctica 25

--- js/scriptpr0325.js
+++ js/scriptpr0325.js
@@ -1,5 +1,45 @@
-let modulos = [];
+let datos = [
+    {
+        nombre: 'Pepe',
+        ape1: 'Pérez',
+        ape2: 'López',
+        dni: '10100100',
+        expediente: '1720',
+        pass: '1234ABC',
+        ciclo: 'DAW',
+        notas: {
+            DWEC: 7.8,
+            DIW: 5.4,
+            DWEC: 9.4
+        }
+    },
+    {
+        nombre: 'Juan',
+        ape1: 'Mázquez',
+        ape2: 'Hernández',
+        dni: '7340831',
+        expediente: '342',
+        pass: 'P@ssw0rd',
+        ciclo: 'DAW',
+        notas: {
+            DWEC: 8,
+            DIW: 5.2,
+            DWES: 4
+        }
+    }
+]
+console.log(getAverageGrade('DIW'));
 
 function getAverageGrade(str) {
-    
+    let media = "El módulo no existe";
+    if (str == "DWEC" || str == "DIW" || str == "DWES") {
+        media = 0;
+        let numeroTotal = 0;
+        datos.forEach( item => {
+            media += item.notas[str];
+            numeroTotal++;
+        } );
+        media = media.toFixed(2) / numeroTotal;
+    }
+    return media;
 }
