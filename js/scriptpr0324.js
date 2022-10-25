e643dd566a8c2f2823bef476cc09a827654030b5
Cecilia Llamazares Lopez <ceciliallamazares99@gmail.com>
2022-10-25 09:03:42 +0200

    Unidad 3 cambios en la práctica 24

--- js/scriptpr0324.js
+++ js/scriptpr0324.js
@@ -1,15 +1,33 @@
 let datos = [
     {
-        nombre: 'Ivan',
-        apellidos: 'Braginski',
-        dni: '71995496',
+        nombre: 'Pepe',
+        ape1: 'Pérez',
+        ape2: 'López',
+        dni: '10100100',
+        expediente: '1720',
+        pass: '1234ABC',
+        ciclo: 'DAW',
+        notas: {
+            DWEC: 7.8,
+            DIW: 5.4,
+            DWEC: 9.4
+        }
     },
     {
-        nombre: 'Antonio',
-        apellidos: 'Fernández Carriedo',
-        dni: '02994386T',
-    },
-];
+        nombre: 'Juan',
+        ape1: 'Mázquez',
+        ape2: 'Hernández',
+        dni: '7340831',
+        expediente: '342',
+        pass: 'P@ssw0rd',
+        ciclo: 'DAW',
+        notas: {
+            DWEC: 8,
+            DIW: 5.2,
+            DWES: 4
+        }
+    }
+]
 console.log(addNif(datos));
 
 function addNif(arr) {
---
+++ js/scriptpr0325.js
@@ -0,0 +1,5 @@
+let modulos = [];
+
+function getAverageGrade(str) {
+    
+}
