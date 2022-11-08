249534fc0b6cebaa69972cbdf863ca0ce6c3a9ea
Cecilia Llamazares Lopez <ceciliallamazares99@gmail.com>
2022-11-08 08:45:19 +0100

    Cambios en las prácticas 1 y 2 u4

--- js/ut04/scriptpr0401.js
+++ js/ut04/scriptpr0401.js
@@ -1,7 +1,15 @@
-let cadena = "";
+/* let cadena = "";
 for(let i = 1; i <= 30; i++) {
     cadena = `${ cadena }<span class="number">${ i }</span>`
 }
 
 let numbers = document.getElementById('numbers');
-numbers.innerHTML = cadena;
+numbers.innerHTML = cadena; */
+
+let div = document.getElementById('numbers');
+for(let i = 1; i <= 30; i++) {
+    let span = document.createElement('span');
+    span.textContent = i;
+    span.classList.add('number');
+    div.append(span);
+}
--- js/ut04/scriptpr0402.js
+++ js/ut04/scriptpr0402.js
@@ -1,4 +1,4 @@
-let cadena = "";
+/* let cadena = "";
 let meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
 for(let i = 1; i <= 12; i++) {
     cadena = `${ cadena }
@@ -11,4 +11,23 @@ for(let i = 1; i <= 12; i++) {
 
 let divMeses = document.getElementById('months');
 divMeses.innerHTML = cadena;
-console.log(cadena);
+console.log(cadena); */
+
+let meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+let div = document.getElementById('months');
+for(let i = 1; i <= 12; i++) {
+    let divMonth = document.createElement('div');
+    divMonth.classList.add('month');
+
+    let monthNumber = document.createElement('div');
+    monthNumber.classList.add('month-number');
+    monthNumber.textContent = i;
+
+    let monthName = document.createElement('div')
+    monthName.classList.add('month-name');
+    monthName.textContent = meses[i-1];
+    
+    divMonth.append(monthNumber);
+    divMonth.append(monthName);
+    div.append(divMonth);
+}
